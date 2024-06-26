ae6d76a4bed81c82d755157f562cb76cd4420b4a
kawano <kyosuke.1215@icloud.com>
2024-06-26 10:48:54 +0900

    初回

---
+++ src/main/java/com/example/demo/ApplicationInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationInsightsApplication.class, args);
+	}
+
+}
